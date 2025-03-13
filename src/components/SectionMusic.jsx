370a65d85cad2eb4cb5877ae961aa34badad40ba
CopyCatMe <wiskripper@gmail.com>
2025-03-13 02:03:38 +0100

    feat: adjust height calculation in SectionMusic component for improved layout

--- src/components/SectionMusic.jsx
+++ src/components/SectionMusic.jsx
@@ -13,7 +13,7 @@ function SectionMusic({ isOpen, onSelectSong, songs, setSongs }) {
 
     return (
         <div className={`transition-all duration-300 w-[80%] ${isOpen ? '' : 'ml-[-200px]'}`}>
-            <div className="w-full overflow-y-scroll h-[calc(100vh-130px)]" style={{ scrollbarWidth: 'none' }}>
+            <div className="w-full overflow-y-scroll h-[calc(100vh-150px)]" style={{ scrollbarWidth: 'none' }}>
                 <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 2xl:grid-cols-7 gap-6 m-1 pb-15 sm:pb-20 pt-5 transition-all duration-300">
                     {songs.slice(0, 50).map((song) => (
                         <div
